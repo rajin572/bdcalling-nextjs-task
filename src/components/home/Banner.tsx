25f5de170da01dd14069af79436ed620df5ccd39
din-islam-rajin <din.islam.rajin572@gmail.com>
2024-07-12 00:48:28 +0600

    Design Complete

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -16,6 +16,21 @@ import {
   CarouselPrevious,
 } from "../ui/carousel";
 
+const bannerFoodImages = [
+  {
+    image: bannerImg1.src,
+  },
+  {
+    image: bannerImg2.src,
+  },
+  {
+    image: bannerImg3.src,
+  },
+  {
+    image: bannerImg4.src,
+  },
+];
+
 const Banner = () => {
   return (
     <div
@@ -50,34 +65,36 @@ const Banner = () => {
           <div>
             <div className="bg-gradient-to-b from-secondary to-green-400 w-[280px] h-[280px] md:w-[500px] md:h-[500px] rounded-full flex justify-center items-center">
               <Carousel className="w-[280px] md:w-[500px]">
-                <CarouselContent>
-                  <CarouselItem>
-                    <div className="w-[220px] h-[220px] md:w-[410px] md:h-[400px] mx-auto bg-white rounded-full flex justify-center items-center">
-                      <Image
-                        src={bannerImg1.src}
-                        alt="food_image"
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="w-[90%] h-[90%] object-cover"
-                      />
-                    </div>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <div className="w-[220px] h-[220px] md:w-[410px] md:h-[400px] mx-auto bg-white rounded-full flex justify-center items-center">
-                      <Image
-                        src={bannerImg2.src}
-                        alt="food_image"
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="w-[90%] h-[90%] object-cover"
-                      />
-                    </div>
-                  </CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious className="ml-10 text-sm md:ml-14" />
-                <CarouselNext className="mr-10 text-sm md:mr-14" />
+                <div
+                  className="w-[220px] h-[220px] md:w-[400px] md:h-[400px] mx-auto rounded-full flex justify-center items-center shadow-inner shadow-zinc-700 overflow-hidden"
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)),url(${backgorundImg.src})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundOrigin: "contentBox",
+                    backgroundSize: "cover",
+                  }}
+                >
+                  <CarouselContent>
+                    {bannerFoodImages.map((image, index) => (
+                      <CarouselItem
+                        key={index}
+                        className="flex justify-center items-center"
+                      >
+                        <Image
+                          src={image.image}
+                          alt="food_image"
+                          width={0}
+                          height={0}
+                          sizes="100vw"
+                          className="w-[150px] h-[150px] md:w-[300px] md:h-[300px] object-cover mx-auto"
+                        />
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                </div>
+                <CarouselPrevious className="ml-12 text-sm md:ml-14" />
+                <CarouselNext className="mr-12 text-sm md:mr-14" />
               </Carousel>
             </div>
           </div>
